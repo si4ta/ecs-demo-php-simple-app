682b20db99c28acbdc4ab4af97772f829b60b3f6
si4ta <nufan00000@gmail.com>
2018-10-29 17:07:09 +0900

    test

--- src/index.php
+++ src/index.php
@@ -24,7 +24,7 @@
                         echo fread($myfile,filesize("/var/www/my-vol/date"));
                         fclose($myfile);
                 ?>
-
+test
             </div>
         </div>
 
